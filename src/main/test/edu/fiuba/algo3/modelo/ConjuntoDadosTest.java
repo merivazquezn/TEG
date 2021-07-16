ae898639f9d319fcbf80de31b69f20fb2e1d9304
MateoBulnes <mbulnes@fi.uba.ar>
2021-07-16 16:50:15 -0300

    solucionado problema con constantes de dado

--- src/main/test/edu/fiuba/algo3/modelo/ConjuntoDadosTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/ConjuntoDadosTest.java
@@ -13,6 +13,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+
+
 public class ConjuntoDadosTest {
 
     @Test
