bfa5efbda08d305c708eb84e1f2bb33654f84f27
MateoBulnes <mbulnes@fi.uba.ar>
2021-07-17 12:39:23 -0300

    agregada funcionalidad de ronda colocacion

---
+++ src/main/java/edu/fiuba/algo3/modelo/flujoDeJuego/RondaColocacion.java
@@ -0,0 +1,29 @@
+package edu.fiuba.algo3.modelo.flujoDeJuego;
+import edu.fiuba.algo3.controlador.Comunicacion;
+import edu.fiuba.algo3.controlador.EleccionColocacion;
+import edu.fiuba.algo3.modelo.general.Tablero;
+import edu.fiuba.algo3.modelo.jugador.Jugador;
+
+import java.util.ArrayList;
+
+public class RondaColocacion implements Ronda {
+
+    private ArrayList<Jugador> jugadores;
+    private EleccionColocacion eleccionColocacion;
+
+    public RondaColocacion(ArrayList <Jugador> jugadores, Comunicacion comunicacion){
+        this.jugadores = jugadores;
+        this.eleccionColocacion = comunicacion.getEleccionColocacion();
+    }
+
+    public boolean realizarRondaYContinuar(Tablero tablero){
+        int i = 0;
+        while(i < this.jugadores.size()){
+            TurnoColocacion turnoColocacion = new TurnoColocacion(this.jugadores.get(i), this.eleccionColocacion);
+            turnoColocacion.realizarTurnoYContinuar(tablero);
+            i++;
+        }
+        return true;
+    }
+
+}
---
+++ src/main/java/edu/fiuba/algo3/modelo/flujoDeJuego/TurnoColocacion.java
@@ -0,0 +1,43 @@
+package edu.fiuba.algo3.modelo.flujoDeJuego;
+
+import edu.fiuba.algo3.controlador.EleccionAtaque;
+import edu.fiuba.algo3.controlador.EleccionColocacion;
+import edu.fiuba.algo3.modelo.ataque.ConstructorDeConjuntoDados;
+import edu.fiuba.algo3.modelo.general.Pais;
+import edu.fiuba.algo3.modelo.general.Tablero;
+import edu.fiuba.algo3.modelo.jugador.*;
+
+import java.util.ArrayList;
+
+public class TurnoColocacion implements Turno{
+
+    private Jugador jugador;
+    private EleccionColocacion eleccion;
+
+    public TurnoColocacion(Jugador unJugador, EleccionColocacion eleccion){
+        this.jugador = unJugador;
+        this.eleccion = eleccion;
+    }
+
+    public boolean realizarTurnoYContinuar(Tablero tablero){
+        int cantidadAColocar = this.jugador.cantidadPaises();
+        cantidadAColocar /= 2;
+        cantidadAColocar += tablero.cantidadEjercitosPorContinente(this.jugador);
+        while(cantidadAColocar > 0){
+            if(eleccion.canjeoDeTarjetas()){
+                Tarjeta tarjetaACanjear = eleccion.tarjetaACanjear();
+                tarjetaACanjear.activar();
+            }
+            else {
+                ArrayList<Object> listaElecciones = this.eleccion.ejercitosAColocarEnPais();
+                Pais paisAColocar = (Pais) listaElecciones.get(0);
+                int cantidadPedida = (int) listaElecciones.get(1);
+                if (cantidadPedida <= cantidadAColocar) {
+                    paisAColocar.colocarEjercitos(cantidadPedida);
+                    cantidadAColocar -= cantidadPedida;
+                }
+            }
+        }
+        return true;
+    }
+}
