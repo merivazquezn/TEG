39b96e6c53a563a8205c2be566679968e819acb3
Franco <franco28xl@gmail.com>
2021-07-21 14:00:17 -0300

    v0.0.5

--- src/main/test/edu/fiuba/algo3/modelo/jugador/SignoTest.java
+++ src/main/test/edu/fiuba/algo3/modelo/jugador/SignoTest.java
@@ -1,4 +1,4 @@
-package edu.fiuba.algo3.modelo;
+package edu.fiuba.algo3.modelo.jugador;
 
 import edu.fiuba.algo3.modelo.jugador.IdentificadorInvalidoException;
 import edu.fiuba.algo3.modelo.jugador.Signo;
@@ -11,7 +11,7 @@ public class SignoTest {
     public void test01SignoDevuelveIdentificadorInvalidoExceptionSiElIdentificadorNoEstaEntre0y2() {
 
         assertThrows(IdentificadorInvalidoException.class, () -> {
-            Signo signo = new Signo(-1);
+            Signo signo = new Signo(-2);
         });
 
         assertThrows(IdentificadorInvalidoException.class, () -> {
