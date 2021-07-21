3e49cc639b4fffb0428ca9891bb976c4c2f51619
Franco <franco28xl@gmail.com>
2021-07-21 13:55:43 -0300

    changes by maria

--- src/main/java/edu/fiuba/algo3/modelo/jugador/ObjetivoCantidadPorContinente.java
+++ src/main/java/edu/fiuba/algo3/modelo/jugador/ObjetivoCantidadPorContinente.java
@@ -3,21 +3,21 @@ package edu.fiuba.algo3.modelo.jugador;
 import edu.fiuba.algo3.modelo.general.Tablero;
 
 import java.util.ArrayList;
-import java.util.Map;
+import java.util.HashMap;
 
 public class ObjetivoCantidadPorContinente implements Objetivo{
 
     private Jugador jugador;
-    private Map<String, Integer> objetivo;
+    private HashMap<String, Integer> objetivo;
 
-    public ObjetivoCantidadPorContinente(Jugador unJugador, Map<String, Integer> objetivo) {
+    public ObjetivoCantidadPorContinente(Jugador unJugador, HashMap<String, Integer> objetivo) {
         this.jugador = unJugador;
         this.objetivo = objetivo;
     }
 
     public boolean haGanado(Tablero tablero, ArrayList<Jugador> listaJugadores){
         boolean cumpleObjetivo = true;
-        for (Map.Entry<String, Integer> entry : this.objetivo.entrySet()) {
+        for (HashMap.Entry<String, Integer> entry : this.objetivo.entrySet()) {
             String nombreContinente = entry.getKey();
             Integer cantidad = entry.getValue();
             if(tablero.cantidadPaisesDeJugadorEn(this.jugador, nombreContinente) < cantidad)
