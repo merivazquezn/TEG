5e377b06183ad717a35947784fde16632aeee53a
merivazquezn <mvazquezn@fi.uba.ar>
2021-07-24 18:31:37 -0300

    ConjuntoTarjetas implementado

--- src/main/java/edu/fiuba/algo3/modelo/general/ConjuntoTarjetas.java
+++ src/main/java/edu/fiuba/algo3/modelo/general/ConjuntoTarjetas.java
@@ -1,4 +1,33 @@
 package edu.fiuba.algo3.modelo.general;
 
+import edu.fiuba.algo3.modelo.ataque.CantidadInvalidaDeDadosError;
+
 public class ConjuntoTarjetas {
+    private Tarjeta tarjeta1;
+    private Tarjeta tarjeta2;
+    private Tarjeta tarjeta3;
+    private Mazo mazo;
+
+    public ConjuntoTarjetas(Tarjeta tarjeta1, Tarjeta tarjeta2, Tarjeta tarjeta3, Mazo mazo){
+        this.tarjeta1 = tarjeta1;
+        this.tarjeta2 = tarjeta2;
+        this.tarjeta3 = tarjeta3;
+        this.mazo = mazo;
+    }
+    private boolean sonCanjeables(){
+        if(this.tarjeta1.mismoSignoQue(this.tarjeta2) && this.tarjeta1.mismoSignoQue(this.tarjeta3))
+            return true;
+        else if(!this.tarjeta1.mismoSignoQue(this.tarjeta2) && !this.tarjeta1.mismoSignoQue(this.tarjeta3) && !this.tarjeta2.mismoSignoQue(this.tarjeta3))
+            return true;
+        else
+            throw new TarjetasNoCanjeablesError();
+    }
+
+    public boolean sePudoCanjear(){
+        if(this.sonCanjeables()){
+            //this.mazo.agregarTarjetas(this.tarjeta1, this.tarjeta2, this.tarjeta3));
+            return true;
+        }
+        return false;
+    }
 }
---
+++ src/main/test/edu/fiuba/algo3/modelo/general/ConjuntoTarjetasTest.java
@@ -0,0 +1,60 @@
+package edu.fiuba.algo3.modelo.general;
+
+import edu.fiuba.algo3.infraestructura.Randomizador;
+import edu.fiuba.algo3.modelo.ataque.CantidadInvalidaDeDadosError;
+import edu.fiuba.algo3.modelo.ataque.ConjuntoDados;
+import edu.fiuba.algo3.modelo.ataque.ConstructorDeConjuntoDados;
+import edu.fiuba.algo3.modelo.flujoDeJuego.JugadaCanjearTarjetas;
+import edu.fiuba.algo3.modelo.flujoDeJuego.Ronda;
+import edu.fiuba.algo3.modelo.jugador.Jugador;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ConjuntoTarjetasTest {
+
+    //0 para Barco; 1 para Globo; 2 para Cañon
+    @Test
+    public void test01SeCanjean3CartasDeTipoGloboYSonCanjeables(){
+        Mazo mazo = new Mazo(new ArrayList<>(), new Randomizador());
+
+        Tarjeta tarjeta1 = new Tarjeta(new Pais("Argentina"), new Signo(1));
+        Tarjeta tarjeta2 = new Tarjeta(new Pais("Brasil"), new Signo(1));
+        Tarjeta tarjeta3 = new Tarjeta(new Pais("Chile"), new Signo(1));
+
+        ConjuntoTarjetas conjuntoTarjetas = new ConjuntoTarjetas(tarjeta1, tarjeta2, tarjeta3, mazo);
+        assertTrue(conjuntoTarjetas.sePudoCanjear());
+    }
+
+    @Test
+    public void test02SeCanjean2CartasDeTipoGloboYUnaDeTipoCanonYNoSonCanjeables(){
+        Mazo mazo = new Mazo(new ArrayList<>(), new Randomizador());
+
+        Tarjeta tarjeta1 = new Tarjeta(new Pais("Argentina"), new Signo(1));
+        Tarjeta tarjeta2 = new Tarjeta(new Pais("Brasil"), new Signo(1));
+        Tarjeta tarjeta3 = new Tarjeta(new Pais("Chile"), new Signo(2));
+
+        ConjuntoTarjetas conjuntoTarjetas = new ConjuntoTarjetas(tarjeta1, tarjeta2, tarjeta3, mazo);
+        assertThrows(TarjetasNoCanjeablesError.class, () -> {
+            conjuntoTarjetas.sePudoCanjear();
+        });
+    }
+
+    @Test
+    public void test03SeCanjean3CartasDeDistintoTipoYSonCanjeables(){
+        Mazo mazo = new Mazo(new ArrayList<>(), new Randomizador());
+
+        Tarjeta tarjeta1 = new Tarjeta(new Pais("Argentina"), new Signo(0));
+        Tarjeta tarjeta2 = new Tarjeta(new Pais("Brasil"), new Signo(1));
+        Tarjeta tarjeta3 = new Tarjeta(new Pais("Chile"), new Signo(2));
+
+        ConjuntoTarjetas conjuntoTarjetas = new ConjuntoTarjetas(tarjeta1, tarjeta2, tarjeta3, mazo);
+        assertTrue(conjuntoTarjetas.sePudoCanjear());
+    }
+
+
+}
